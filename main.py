92b14a9a1343fdb84e19b9e9e6bd46b6aadec7e8
ljinlabs <ljinlabs@gmail.com>
2022-09-09 13:07:38 +0900

    update todo

---
+++ main.py
@@ -0,0 +1,16 @@
+# todo
+# 1. password generating algorithm
+# 1.1 get user input? what input?
+# 2. show generated password
+# 3. get user input. -> yes or no
+#
+# sql
+# 4. create database
+# 5. check input valid
+# 6. store data
+#
+# fetch
+# 7. get password by website
+# 
+# advanced
+# 8. password filtering
